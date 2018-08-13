c7a87942d67fc91e11baa93d9f22548732c38d7f
andrewMacmurray <a.macmurray@icloud.com>
2018-08-13 13:38:15 +0100

    adds logging middleware spec

---
+++ spec/clj_http_server/middleware/logging_spec.clj
@@ -0,0 +1,28 @@
+(ns clj-http-server.middleware.logging-spec
+  (:require [speclj.core :refer :all]
+            [clj-http-server.utils.logging :refer :all]
+            [clj-http-server.middleware.logging :refer :all]))
+
+(defn handler [request]
+  {:status 200})
+
+(def modified-handler
+  (with-logs handler))
+
+(describe "with-logs"
+          (with-stubs)
+          (around [it] (with-redefs [log-request (stub :log-request-stub)]))
+          (it "calls the logger with the request when handler is called"
+              (let [request {:uri "/hello"}
+                    _ (modified-handler request)]
+                (should-have-invoked :log-request-stub {:with [request]})))
+
+          (it "ignores logs when request uri is /"
+              (let [request {:uri "/"}
+                    _ (modified-handler request)]
+                (should-not-have-invoked :log-request-stub)))
+
+          (it "does not modify the request or response"
+              (let [request {:uri "/hello"}
+                    response (modified-handler)]
+                (should= (handler request) response))))
